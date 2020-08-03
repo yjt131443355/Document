6858d4a6c0d749a1eb8ff30d6a4ac9a2bd92065d
yjt131443355 <564708969@qq.com>
2020-08-03 22:07:03 +0800

    修改远程冲突文件

--- Test.cpp
+++ Test.cpp
@@ -5,16 +5,6 @@ enum Week{
 };
 
 int main(void){
-<<<<<<< Updated upstream
-<<<<<<< HEAD
     Week today = TWO;
-=======
-    
->>>>>>> 27e367d12b2b076488bde3390c60f6e1bb9a737b
-    
     return 0;
 }
-=======
-    Week today = TWO;
-}
->>>>>>> Stashed changes
